03bdb2654ec453546cb8d3a5ff30d667f481e569
Sana Rodolfo <sana.rodolfo@code.berlin>
2022-04-02 10:45:12 +0200

    Apply factory pattern

--- app/app.py
+++ app/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from . import scrollers, basic_pages
 
-app = Flask(__name__)
-app.config.from_object('app.config')
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config')
+
+    register_blueprints(app)
+
+    return app
 
 # Blueprints
-app.register_blueprint(scrollers.routes.blueprint)
-app.register_blueprint(basic_pages.routes.blueprint)
+def register_blueprints(app: Flask):
+    app.register_blueprint(scrollers.routes.blueprint)
+    app.register_blueprint(basic_pages.routes.blueprint)
