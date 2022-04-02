00960cb6791f5a1b61726312b9f322f81945b663
Sana Rodolfo <sana.rodolfo@code.berlin>
2022-04-02 12:02:38 +0200

    Add logging to file and console

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from . import scrollers, basic_pages
+import logging
 
 def create_app():
     app = Flask(__name__)
@@ -13,3 +14,27 @@ def create_app():
 def register_blueprints(app: Flask):
     app.register_blueprint(scrollers.routes.blueprint)
     app.register_blueprint(basic_pages.routes.blueprint)
+
+# Logging
+
+# Creating an object
+logger = logging.getLogger()
+
+# Setting the threshold of logger to DEBUG
+logger.setLevel(logging.DEBUG)
+
+# Output to file settings
+logfile = logging.FileHandler('file.log')
+logfile.setLevel(logging.INFO)
+logfileformat = logging.Formatter('%(asctime)s - %(name)s - [%(filename)s > %(funcName)s() > %(lineno)s] - %(levelname)s - %(message)s')
+logfile.setFormatter(logfileformat)
+
+logger.addHandler(logfile)
+
+# Output to console setting
+stream = logging.StreamHandler()
+stream.setLevel(logging.DEBUG)
+streamformat = logging.Formatter('[%(levelname)s] %(message)s')
+stream.setFormatter(streamformat)
+
+logger.addHandler(stream)
