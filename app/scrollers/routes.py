88d83bb69337adaa75f2833947a1dec58f66c5a6
Sana Rodolfo <sana.rodolfo@code.berlin>
2022-04-09 12:28:24 +0200

    Remove dummy data from scroller routes

--- app/scrollers/routes.py
+++ app/scrollers/routes.py
@@ -1,11 +1,6 @@
 from flask import Blueprint, render_template
 from .models import Scroller, Customhaiku, Defaulthaiku, Mood, Longmessage
 
-scroller_dummy_data = {
-    'sarah': {'to-recipient-name': 'sarah', 'scroller-style': 'spring', 'scroller-colour': 'pastel', 'default-message': True, 'line-1': 'spring peeks through the clouds', 'line-2': 'I am reminded of you', 'line-3': 'and smile. you\'re the sun', 'long-message': 'wishing you well from across the way. hope to see you soon!', 'from-sender-name': 'you know who'},
-    'laura': {'to-recipient-name': 'laura', 'scroller-style': 'spring', 'scroller-colour': 'pastel', 'default-message': True, 'line-1': 'spring peeks through the clouds', 'line-2': 'I am reminded of you', 'line-3': 'and smile. you\'re the sun', 'long-message': 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'from-sender-name': 'you know who'}
-}
-
 blueprint = Blueprint('scrollers', __name__)
 
 @blueprint.route('/view/<slug>')
