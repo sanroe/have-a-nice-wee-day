0fdf87024de3104bfcc5e0e9fe6344a7e1718b57
Sana Rodolfo <sana.rodolfo@code.berlin>
2022-04-09 14:14:57 +0200

    Update create scroller logic to save form data to db

--- app/scrollers/routes.py
+++ app/scrollers/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request
 from .models import Scroller, Customhaiku, Defaulthaiku, Mood, Longmessage
+import secrets
+import string
 
 blueprint = Blueprint('scrollers', __name__)
 
@@ -19,9 +21,41 @@ def get_create():
     return render_template('scrollers/create.html')
 
 @blueprint.post('/create')
-def post_create():
-    # Create a scroller
-    scroller = Scroller()
+def post_create():    
+    # Create a unique slug
+    slug_prefix = request.form.get('to-recipient-name')
+    slug_prefix_clean = slug_prefix.replace(" ", "-")
+    slug_suffix = ''.join(secrets.choice(string.ascii_uppercase + string.ascii_lowercase) for i in range(20))
+    slug = slug_prefix_clean + '-' + slug_suffix
+    
+    # Create a new scroller
+    scroller = Scroller(
+        slug=slug,
+        to_recipient_name=request.form.get('to-recipient-name'),
+        from_sender_name=request.form.get('from-sender-name'),
+        mood_id=request.form.get('mood')
+    )
+    scroller.save()
+
+    # Either create custom haiku entry or update scroller with default
+    if request.form.get('default-message') == "False":
+        customhaiku = Customhaiku(
+            line_one=request.form.get('line-one'),
+            line_two=request.form.get('line-two'),
+            line_three=request.form.get('line-three')
+        )
+        customhaiku.save()
+        scroller.customhaiku_id = customhaiku.id
+        scroller.save()
+    elif request.form.get('default-message') == "True":
+        scroller.defaulthaiku_id = request.form.get('mood')
+        scroller.save()
+
+    longmessage = Longmessage(
+        msg=request.form.get('long-message')
+        )
+    longmessage.save()
+    scroller.longmessage_id = longmessage.id
     scroller.save()
 
     return 'it was posted'
