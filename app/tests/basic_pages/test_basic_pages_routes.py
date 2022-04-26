38a70875f823c1f545d26e855a08ec6df7c04fa1
Sana Rodolfo <sana.rodolfo@code.berlin>
2022-04-26 17:03:38 +0200

    Add page load and return content tests for legal blueprint

--- app/tests/basic_pages/test_basic_pages_routes.py
+++ app/tests/basic_pages/test_basic_pages_routes.py
@@ -17,3 +17,13 @@ def test_about_content(client):
     # Returns about text
     response = client.get('/about')
     assert 'philosophy' in response.get_data(as_text=True)
+
+def test_legal_success(client):
+    # Page loads
+    response = client.get('/legal')
+    assert response.status_code == 200
+
+def test_legal_content(client):
+    # Returns legal text
+    response = client.get('/legal')
+    assert 'disclaimer' in response.get_data(as_text=True)
